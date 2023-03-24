62dec3ce0ed30309a9d7230a66bfbaf61f3eaa3c
anastasistepp <naaaaasta@gmail.com>
2023-03-24 19:22:09 +0400

    1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello')
+import os
+import sqlite3
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+conn = sqlite3.connect(DATABASE_URL, uri=True)
+cursor = conn.cursor()
+
+# создание таблицы
+cursor.execute('''CREATE TABLE stocks
+                 (date text, trans text, symbol text, qty real, price real)''')
+# вставка данных в таблицу
+cursor.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+# сохранение изменений
+conn.commit()
+# закрытие соединения
+conn.close()
